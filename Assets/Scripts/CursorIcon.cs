17fa01c0e177642aded3cd0136b02a7019514cdc
medhahegde04 <medhahegde39@gmail.com>
2026-07-20 23:50:09 +0530

    ui: add dynamic cursor changes and pointer state handling to menu buttons

--- Assets/Scripts/CursorIcon.cs
+++ Assets/Scripts/CursorIcon.cs
@@ -1,41 +1,71 @@
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class CursorIcon : MonoBehaviour
 {
+    public Sprite defaultCursor;
+    public Sprite pointingCursor;
     private RectTransform rectTransform;
     private UnityEngine.UI.Image cursorImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // This stops Unity from destroying the cursor when changing scenes
-
         rectTransform = GetComponent<RectTransform>();
         cursorImage = GetComponentInChildren<UnityEngine.UI.Image>();
 
         Cursor.visible = false;
-
         DontDestroyOnLoad(transform.root.gameObject);
 
         SceneManager.sceneLoaded += OnSceneLoaded;
         CheckCursorVisibility(SceneManager.GetActiveScene().name);
+
+        ResetCursorTexture();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Safety check to make sure both components exist before tracking position
+
         if (cursorImage != null && cursorImage.enabled && rectTransform != null && Mouse.current != null)
         {
             rectTransform.position = Mouse.current.position.ReadValue();
         }
     }
 
+    public void SetHoverState(bool isHovering)
+    {
+        // Change the cursor icon to the pointer sprite
+
+        if (cursorImage == null) return;
+
+        if (isHovering && pointingCursor != null)
+        {
+            cursorImage.sprite = pointingCursor;
+        }
+        else
+        {
+            ResetCursorTexture();
+        }
+    }
+
+    private void ResetCursorTexture()
+    {
+        // Resets the cursor to default sprite
+
+        if (cursorImage != null && defaultCursor != null)
+        {
+            cursorImage.sprite = defaultCursor;
+        }
+    }
+
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         CheckCursorVisibility(scene.name);
+        ResetCursorTexture();
     }
    
     private void CheckCursorVisibility(string sceneName)
